442a6297f1fcf469814c1480f00e9091c1f54605
Ben Gordon <Bengordon1995@gmail.com>
2020-11-05 09:32:51 -0600

    added express validator to login routes

--- Routes/Users.js
+++ Routes/Users.js
@@ -2,7 +2,7 @@ const router = require("express").Router()
 const db = require('../Models')
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken")
-
+const { body, validationResult } = require('express-validator');
 const jwtKey = process.env.JWT_KEY
 const jwtExpiry = 1 * 60 * 60 * 24 * 7 // one week valid token;
 
@@ -13,37 +13,51 @@ router.get('/login', (req,res) => {
 
 
 // LOGIN
-router.post('/login', async (req, res) => {
-    try{
-        const {email, password} = req.body
-        const foundUser = await db.User.findOne({email:email})
-        if (!email || !password){
-            return res.redirect("/login")
+router.post('/login', [
+        body('email').isEmail(),
+        body('password').isLength({min:8})
+    ], async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()){
+            res.redirect("/")
         }
-        else {
-            const isValidPassword = await bcrypt.compare(password, foundUser.password)
-            if (!isValidPassword){
-                return res.sendStatus(401)
+        try{
+            const {email, password} = req.body
+            const foundUser = await db.User.findOne({email:email})
+            if (!email || !password){
+                return res.redirect("/login")
             }
-            const token = jwt.sign({ email }, jwtKey, {
-                algorithm: "HS256",
-                expiresIn: jwtExpiry,
-            })
-            res.cookie("token", token, {maxAge: jwtExpiry * 1000})
+            else {
+                const isValidPassword = await bcrypt.compare(password, foundUser.password)
+                if (!isValidPassword){
+                    return res.sendStatus(401)
+                }
+                const token = jwt.sign({ email }, jwtKey, {
+                    algorithm: "HS256",
+                    expiresIn: jwtExpiry,
+                })
+                res.cookie("token", token, {maxAge: jwtExpiry * 1000})
 
-            res.redirect("/rooms")
+                res.redirect("/rooms")
+            }
         }
-    }
-    catch(err){
-        console.log(err)
-        res.json({message: "Error logging in"})
-    }
+        catch(err){
+            console.log(err)
+            res.json({message: "Error logging in"})
+        }
 })
 
 
 
 // REGISTER
-router.post("/register", async (req, res) => {
+router.post("/register", [
+        body('email').isEmail(),
+        body('password').isLength({min:8})
+    ], async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()){
+            res.redirect("/")
+        }
     try{
         const {username, password, email} = req.body
         if (!password || !email){
